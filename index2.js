224e43ed99e8a6549598760c60ba94bd4e9331ea
johncabang <johncabz@gmail.com>
2020-09-26 02:03:20 +0000

    Moduralize printPassTimes

--- index2.js
+++ index2.js
@@ -2,15 +2,7 @@ const { fetchMyIP } = require('./iss_promised');
 const { fetchCoordsByIP } = require('./iss_promised');
 const { fetchISSFlyOverTimes } = require('./iss_promised');
 const { nextISSTimesForMyLocation } = require('./iss_promised');
-
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
+const { printPassTimes } = require('./index');
 
 fetchMyIP()
   .then(fetchCoordsByIP)
--- iss_promised.js
+++ iss_promised.js
@@ -1,17 +1,9 @@
 const request = require('request-promise-native');
-/*
- * Requests user's ip address from https://www.ipify.org/
- * Input: None
- * Returns: Promise of request for ip data, returned as JSON string
- */
+
 const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
-/*
- * Makes a request to ipvigilante.com using the provided IP address, to get its geographical information (latitude/longitude)
- * Input: JSON string containing the IP address
- * Returns: Promise of request for lat/lon
- */
+
 const fetchCoordsByIP = function(body) {
   const ip = JSON.parse(body).ip;
   return request(`https://ipvigilante.com/json/${ip}`);
